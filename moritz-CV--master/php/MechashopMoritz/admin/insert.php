d7ecc40107c919df13fa92e626742c549691415b
tm7 <moritzleter@gmail.com>
2021-05-26 09:58:28 +0200

    Commit to master

---
+++ moritz-CV--master/php/MechashopMoritz/admin/insert.php
@@ -0,0 +1,154 @@
+<?php
+     
+    require 'database.php';
+ 
+    $nameError = $descriptionError = $priceError = $categoryError = $imageError = $name = $description = $price = $category = $image = "";
+
+    if(!empty($_POST)) 
+    {
+        $name               = checkInput($_POST['name']);
+        $description        = checkInput($_POST['description']);
+        $price              = checkInput($_POST['price']);
+        $category           = checkInput($_POST['category']); 
+        $image              = checkInput($_FILES["image"]["name"]);
+        $imagePath          = '../images/'. basename($image);
+        $imageExtension     = pathinfo($imagePath,PATHINFO_EXTENSION);
+        $isSuccess          = true;
+        $isUploadSuccess    = false;
+        
+        if(empty($name)) 
+        {
+            $nameError = 'Dieses Feld ist leer';
+            $isSuccess = false;
+        }
+        if(empty($description)) 
+        {
+            $descriptionError = 'Dieses Feld kann nicht leer bleiben';
+            $isSuccess = false;
+        } 
+        if(empty($price)) 
+        {
+            $priceError = 'Dieses Feld kann nicht leer bleiben';
+            $isSuccess = false;
+        } 
+        if(empty($category)) 
+        {
+            $categoryError = 'Dieses Feld kann nicht leer bleiben';
+            $isSuccess = false;
+        }
+        if(empty($image)) 
+        {
+            $imageError = 'Dieses Feld kann nicht leer bleiben';
+            $isSuccess = false;
+        }
+        else
+        {
+            $isUploadSuccess = true;
+            if($imageExtension != "jpg" && $imageExtension != "png" && $imageExtension != "jpeg" && $imageExtension != "gif" ) 
+            {
+                $imageError = "Les fichiers autorises sont: .jpg, .jpeg, .png, .gif";
+                $isUploadSuccess = false;
+            }
+            if(file_exists($imagePath)) 
+            {
+                $imageError = "Dieses Bild existiert schon";
+                $isUploadSuccess = false;
+            }
+            if($_FILES["image"]["size"] > 500000) 
+            {
+                $imageError = "Dieses Bild muss maximal 5000KB sein";
+                $isUploadSuccess = false;
+            }
+            if($isUploadSuccess) 
+            {
+                if(!move_uploaded_file($_FILES["image"]["tmp_name"], $imagePath)) 
+                {
+                    $imageError = "Es gab einen Fehler während des Upload";
+                    $isUploadSuccess = false;
+                } 
+            } 
+        }
+        
+        if($isSuccess && $isUploadSuccess) 
+        {
+            $db = Database::connect();
+            $statement = $db->prepare("INSERT INTO items (name,description,price,category,image) values(?, ?, ?, ?, ?)");
+            $statement->execute(array($name,$description,$price,$category,$image));
+            Database::disconnect();
+            header("Location: index.php");
+        }
+    }
+
+    function checkInput($data) 
+    {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Mechashop Moritz</title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <link href='http://fonts.googleapis.com/css?family=Holtwood+One+SC' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="../css/styles.css">
+    </head>
+    
+    <body>
+        <h1 class="text-logo"><span class="glyphicon glyphicon-cog"></span> Mechashop Moritz <span class="glyphicon glyphicon-cog"></span></h1>
+         <div class="container admin">
+            <div class="row">
+                <h1><strong>Adden Sie ein Item</strong></h1>
+                <br>
+                <form class="form" action="insert.php" role="form" method="post" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="name">Name:</label>
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Nom" value="<?php echo $name;?>">
+                        <span class="help-inline"><?php echo $nameError;?></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="description">Beschreibung:</label>
+                        <input type="text" class="form-control" id="description" name="description" placeholder="Description" value="<?php echo $description;?>">
+                        <span class="help-inline"><?php echo $descriptionError;?></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="price">Preis: (en €)</label>
+                        <input type="number" step="0.01" class="form-control" id="price" name="price" placeholder="Prix" value="<?php echo $price;?>">
+                        <span class="help-inline"><?php echo $priceError;?></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="category">Kategorie:</label>
+                        <select class="form-control" id="category" name="category">
+                        <?php
+                           $db = Database::connect();
+                           foreach ($db->query('SELECT * FROM categories') as $row) 
+                           {
+                                echo '<option value="'. $row['id'] .'">'. $row['name'] . '</option>';;
+                           }
+                           Database::disconnect();
+                        ?>
+                        </select>
+                        <span class="help-inline"><?php echo $categoryError;?></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="image">Wählen Sie ein Bild aus:</label>
+                        <input type="file" id="image" name="image"> 
+                        <span class="help-inline"><?php echo $imageError;?></span>
+                    </div>
+                    <br>
+                    <div class="form-actions">
+                        <button type="submit" class="btn btn-success"><span class="glyphicon glyphicon-pencil"></span> adden</button>
+                        <a class="btn btn-primary" href="index.php"><span class="glyphicon glyphicon-arrow-left"></span> Zurück</a>
+                   </div>
+                </form>
+            </div>
+        </div>   
+    </body>
+</html>
---
+++ moritz-CV--master/php/MechashopMoritz/moritzmechashop_fini (1)/moritzmechashopcode_fini.sql
@@ -0,0 +1,111 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 26, 2016 at 04:42 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `burgercode_fini`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Menus'),
+(2, 'Burgers'),
+(3, 'Snacks'),
+(4, 'Salades'),
+(5, 'Boissons'),
+(6, 'Desserts');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `price` float NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `category` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `name`, `description`, `price`, `image`, `category`) VALUES
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category` (`category`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `items`
+--
+ALTER TABLE `items`
+  ADD CONSTRAINT `items_ibfk_1` FOREIGN KEY (`category`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
